23530a769f4c7acada3310b2f1b5141023215227
Twan Huiskens <huiskenstwan@gmail.com>
2023-03-27 13:22:53 +0200

    Add searching for specific date ranges and languages

--- download.py
+++ download.py
@@ -14,14 +14,14 @@ class Book(NamedTuple):
     subjects: list[str]
     bookshelves: list[str]
     genre: str
+    date_range: str
 
 
-def _parse_book_json(data: dict[str, Any]) -> Book:
+def _parse_book_json(data: dict[str, Any], genre: str, date: str) -> Book:
 
     text_url = None
     for key in (
             'text/plain; charset=utf-8',
-            'text/plain',
             'text/plain; charset=us-ascii',
     ):
         if key in data['formats']:
@@ -36,7 +36,8 @@ def _parse_book_json(data: dict[str, Any]) -> Book:
         author=data['authors'][0]['name'],  # TODO: Allow multiple auhtors
         subjects=data['subjects'],
         bookshelves=data['bookshelves'],
-        genre='Unknown',  # TODO: Add genre?
+        genre=genre,
+        date_range=date,
     )
 
 
@@ -51,7 +52,7 @@ def _download_book(book: Book, output_dir: str, exist_ok=False) -> None:
     clean_title = book.title.replace(' ', '_')
     clean_author = book.title.replace(' ', '_')
     save_path = os.path.join(
-        output_dir, f'{book.gutenberg_id}_{clean_title}_{clean_author}.txt',
+        output_dir, f'{book.gutenberg_id}_{clean_title}_{clean_author}_{book.date_range}.txt',  # noqa: E501
     )
 
     # Do no download books that are already downloaded
@@ -68,21 +69,27 @@ def _download_book(book: Book, output_dir: str, exist_ok=False) -> None:
 def _get_books_by_genre(
     genre: str,
     page: None | int = None,
-    date_range=None,
-    language='en',
+    author_year_start: int | None = None,
+    author_year_end: int | None = None,
     max_pages: int = 5,
+    language: str | None = None,
 ) -> list[Book]:
-
-    # TODO:
-    #   - Implement date ranges
-    #       (so that books from the same date range can be downloaded)
 
     if page is None:
         page = 1
 
+    r_params = {'topic': genre, 'page': page}
+
+    if author_year_start is not None and author_year_end is not None:
+        r_params['author_year_start'] = author_year_start
+        r_params['author_year_end'] = author_year_end
+
+    if language is not None:
+        r_params['languages'] = language
+
     r = requests.get(
         'https://gutendex.com/books/',
-        params={'topic': genre, 'page': page},  # type: ignore
+        params=r_params,  # type: ignore
     )
     if not r.ok:
         return []
@@ -91,11 +98,11 @@ def _get_books_by_genre(
     book_data: list[dict] = data['results']
 
     # Download all the books (from multiple pages)
-    # TODO: Add TQDM progress bar
     while 'next' in data and page < max_pages:
+        r_params['page'] = page
         r = requests.get(
             'https://gutendex.com/books/',
-            params={'topic': genre, 'page': page},  # type: ignore
+            params=r_params,  # type: ignore
         )
         if not r.ok:
             print('ERROR 1')
@@ -106,7 +113,11 @@ def _get_books_by_genre(
 
     books = []
     for book in book_data:
-        books.append(_parse_book_json(book))
+        books.append(
+            _parse_book_json(
+                book, genre, f'{author_year_start}-{author_year_end}',
+            ),
+        )
 
     return books
 
@@ -115,19 +126,39 @@ def main() -> int:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('genre')
-    parser.add_argument('--max-pages', type=int, default=5)
-    parser.add_argument('--start-page', type=int, default=1)
-    parser.add_argument('--language', type=str, default='en')
+    parser.add_argument(
+        '--max-pages', type=int, default=5,
+        help='The maximum amount of pages to fetch.',
+    )
+    parser.add_argument(
+        '--start-page', type=int, default=1,
+        help='The page number to start with.',
+    )
+    parser.add_argument(
+        '--date-start', type=int,
+        help='The start of the date range to get the books from.',
+    )
+    parser.add_argument(
+        '--date-end', type=int,
+        help='The end of the date range to get the books from.',
+    )
+    parser.add_argument(
+        '--language', type=str, default='en',
+        help='The language of the books.',
+    )
 
     args = parser.parse_args()
 
-    # TODO: Date range
-    print(f'Searching books in genre: {args.genre}')
+    print(
+        f'Searching books in genre: {args.genre}, with start between: {args.date_start} and {args.date_end}',  # noqa: E501
+    )
 
     results = _get_books_by_genre(
         args.genre,
         page=args.start_page,
         max_pages=args.max_pages,
+        author_year_start=args.date_start,
+        author_year_end=args.date_end,
         language=args.language,
     )
 
